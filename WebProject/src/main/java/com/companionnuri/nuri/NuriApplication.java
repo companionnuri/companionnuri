e1fde8da36da7d1864d985abd15ce856f6e27eef
Seungmin Lim <seungmin9066@gmail.com>
2022-02-27 23:56:38 +0900

    #1 [BACK] 백엔드 초기세팅

---
+++ WebProject/src/main/java/com/companionnuri/nuri/NuriApplication.java
@@ -0,0 +1,13 @@
+package com.companionnuri.nuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuriApplication.class, args);
+	}
+
+}
