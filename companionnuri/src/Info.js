74bf5607371852775e92215120a83c1fdecd0b58
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-03-03 18:08:03 +0900

    setting

---
+++ companionnuri/src/Info.js
@@ -0,0 +1,9 @@
+function Info() {
+    return (
+        <div>
+            <h1>123</h1>
+        </div>
+    );
+}
+
+export default Info;
---
+++ companionnuri/src/Region.js
@@ -0,0 +1,9 @@
+function Region() {
+    return (
+        <div>
+            <h1>123</h1>
+        </div>
+    );
+}
+
+export default Region;
