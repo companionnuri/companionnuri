901c3a7159b037786a0334abf6dd0b5511a6be87
Seungmin Lim <seungmin9066@gmail.com>
2022-03-03 21:29:09 +0900

    #6 [추가] 예외 가게를 못찾는 메서드 추가

--- WebProject/src/main/java/com/companionnuri/nuri/map/exception/NuSuchPetStoreException.java
+++ WebProject/src/main/java/com/companionnuri/nuri/map/exception/NuSuchPetStoreException.java
@@ -1,5 +1,19 @@
 package com.companionnuri.nuri.map.exception;
 
 public class NuSuchPetStoreException extends RuntimeException{
+	public NuSuchPetStoreException() {
+		super();
+	}
 
+	public NuSuchPetStoreException(ExceptionMessage message) {
+		super(message.toString());
+	}
+
+	public NuSuchPetStoreException(ExceptionMessage message, Throwable cause) {
+		super(message.toString(), cause);
+	}
+
+	public NuSuchPetStoreException(Throwable cause) {
+		super(cause);
+	}
 }
