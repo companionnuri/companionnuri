01b5a4bd2752903d3235d0135fc147093dfa0042
Seungmin Lim <seungmin9066@gmail.com>
2022-03-03 21:42:33 +0900

    #7 [추가] 가게 Dto 클래스 추가

--- WebProject/src/main/java/com/companionnuri/nuri/map/service/PetStoreDto.java
+++ WebProject/src/main/java/com/companionnuri/nuri/map/service/PetStoreDto.java
@@ -1,4 +1,22 @@
 package com.companionnuri.nuri.map.service;
 
+import com.companionnuri.nuri.map.domain.Address;
+import com.companionnuri.nuri.map.domain.StoreCategory;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class PetStoreDto {
+	private Address address;
+	private StoreCategory category;
+
+	public PetStoreDto() {
+	}
+
+	public PetStoreDto(Address address, StoreCategory category){
+		this.address = address;
+		this.category = category;
+	};
 }
