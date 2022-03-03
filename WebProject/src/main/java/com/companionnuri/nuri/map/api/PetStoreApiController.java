115656be779693acfbb72b2b941b1cf9cd194205
Seungmin Lim <seungmin9066@gmail.com>
2022-03-03 23:29:38 +0900

    #9 [추가] 스토어 api 클래스 추가

--- WebProject/src/main/java/com/companionnuri/nuri/map/api/PetStoreApiController.java
+++ WebProject/src/main/java/com/companionnuri/nuri/map/api/PetStoreApiController.java
@@ -2,9 +2,14 @@ package com.companionnuri.nuri.map.api;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.companionnuri.nuri.map.service.PetStoreService;
+
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class PetStoreApiController {
+
+	private final PetStoreService petStoreService;
+
 }
