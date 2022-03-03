4a53afad47eba6c2f598e35fca19e78e0a0aa40c
Seungmin Lim <seungmin9066@gmail.com>
2022-03-03 21:10:16 +0900

    #4 [추가] 가게db 인터페이스추가

--- WebProject/src/main/java/com/companionnuri/nuri/map/repository/PetStoreRepository.java
+++ WebProject/src/main/java/com/companionnuri/nuri/map/repository/PetStoreRepository.java
@@ -1,6 +1,16 @@
 package com.companionnuri.nuri.map.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.companionnuri.nuri.map.domain.PetStore;
 
 public interface PetStoreRepository {
+	void save(PetStore petStore);
 
+	Optional<PetStore> findById(Long id);
+
+	Optional<PetStore> findByName(String name);
+
+	List<PetStore> findAll();
 }
