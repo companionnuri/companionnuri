1f6fba4004c02452848514b388fdffb309bda81c
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-03-21 12:47:17 +0900

    #19 [추가] 팝업창 생성

--- companionnuri/src/RegionButton.js
+++ companionnuri/src/RegionButton.js
@@ -1,4 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 function RegionButton(props) {
+
+    axios.ajax({
+        url: "./grpc-proxy-server-mkvo6j4wsq-du.a.run.app/v1/regcodes?regcode_pattern=*00000000",
+        type: "GET",
+
+        success: function (response) {
+            makeList(response);
+        },
+        error: function (xhr, status, msg) {
+            console.log("error");
+        },
+    });
+
+    function makeList(data) {
+        // var slist = `<h1>시/군/구</h1>`
+        for (let i = 0; i < data.regcodes.length; i++) {
+            setr(`<h1>${data.regcodes[i].name}</h1>`);
+        }
+    }
+
+    const [r, setr] = useState('');
+
 
     function click() {
         props.setRegion("Seoul")
@@ -17,13 +42,14 @@ function RegionButton(props) {
 
     return (
         <div>
+            {r}
             <button onClick={click}>서울특별시</button>
             <button onClick={click2}>부산광역시</button>
             <button onClick={click3}>대구광역시</button>
-            <button onClick={() => props.setRegion("Daejeon")}>대전광역시</button>
+            {/* <button onClick={() => props.setRegion("Daejeon")}>대전광역시</button>
             <button onClick={() => props.setRegion("Gwangju")}>광주광역시</button>
             <button onClick={() => props.setRegion("Incheon")}>인천광역시</button>
-            <button onClick={() => props.setRegion("Ulsan")}>울산광역시</button>
+            <button onClick={() => props.setRegion("Ulsan")}>울산광역시</button> */}
         </div>
     );
 }
