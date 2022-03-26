38917eeab5f784c9db09b6e25c8f9eda95b55bcb
beeee2 <beeee2@naver.com>
2022-03-26 19:48:31 +0900

    [수정] 검색창 퍼블리싱

--- WebProject/companionnuri/src/Navbar.js
+++ WebProject/companionnuri/src/Navbar.js
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Menubar from "./Menubar";
 
+
 const hd_burger_btn = {
+    border: 'none',
+    backgroundColor : 'transparent',
+    fontSize:'34px',
+    color:'#fcf6f5',
+    padding:0,
+    paddingLeft:'30px',
 }
 
 function Navbar() {
@@ -12,8 +19,8 @@ function Navbar() {
     }
 
   return(
-      <nav className='col-3' style={hd_burger_btn}>
-          <button onClick={buttonClick}><i class="fa-solid fa-bars"></i>{value ? <Menubar /> : null} </button>
+      <nav className='col-3' >
+          <button onClick={buttonClick} style={hd_burger_btn}><i class="fa-solid fa-bars"></i>{value ? <Menubar /> : null} </button>
       </nav>
   )
 }
--- WebProject/companionnuri/src/Searchbar.js
+++ WebProject/companionnuri/src/Searchbar.js
@@ -1,14 +1,41 @@
+import { calculateNewValue } from '@testing-library/user-event/dist/utils';
 import React from 'react';
 // import { FaSearch } from 'react-icons/fa';
 
 const hd_search_box = {
+    marginRight:'30px',
+    width: '400px',
+    borderRadius:'20px',
+    backgroundColor: '#fff',
+    height:'40px',
+    padding:'0 15px 0 10px',
+    display: 'flex',
+    alignItems:'center',
+}
+
+const hd_search_icon = {
+    backgroundColor: 'transparent',
+    border:'none',
+    fontSize: '20px',
+    marginRight: '5px',
+    color: '#101820',
+}
+
+const hd_search_input= {
+    border:'none',
+    fontSize:'18px',
+    height:'40px',
+    width:'calc(100% - 45px)',
 }
 
 function Searchbar() {
     return (
-        <div className='col-3' style={hd_search_box}>
-            <input type="text" placeholder="Search.."></input>
-            {/* <FaSearch size="24" color="red" /> */}
+        <div className='col-3 d-flex justify-content-end'>
+            <div style={hd_search_box}>
+                <button style={hd_search_icon}><i class="fa-solid fa-magnifying-glass"></i></button>
+                <input type="text" placeholder="Search.." style={hd_search_input}></input>
+                {/* <FaSearch size="24" color="red" /> */}
+            </div>
         </div>
     );
 }
