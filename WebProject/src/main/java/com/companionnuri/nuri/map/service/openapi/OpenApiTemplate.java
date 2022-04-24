7449cd0ced9b9c80207c121141640f9bc6a3ec57
Seungmin Lim <seungmin9066@gmail.com>
2022-04-24 16:36:30 +0900

    #9 [추가] OPEN API 를 가져오는 TEMPLATE 추가

---
+++ WebProject/src/main/java/com/companionnuri/nuri/map/service/openapi/OpenApiTemplate.java
@@ -0,0 +1,64 @@
+package com.companionnuri.nuri.map.service.openapi;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+
+import com.companionnuri.nuri.map.domain.Store;
+import com.companionnuri.nuri.map.service.openapi.strategy.ApiStrategy;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OpenApiTemplate implements OpenApi {
+
+
+
+	private final Map<String, ApiStrategy> apiStrategy;
+
+	@Override
+	public List<Store> getData(ApiQuery apiQuery) {
+		try {
+			URL url = new URL(apiQuery.toString());
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Content-type", "application/json");
+
+			BufferedReader rd;
+			if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+				rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			} else {
+				rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+			}
+
+			StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = rd.readLine()) != null) {
+				sb.append(line);
+			}
+			rd.close();
+			conn.disconnect();
+
+			return apiStrategy.get(apiQuery.getApiAddress().getBeanName()).handle(new JSONObject(sb.toString()));
+
+		} catch (Exception e){
+			// API 오류 핸들러를 추가하자.
+			log.error("error", e);
+			throw new IllegalStateException(e);
+		}
+	}
+
+	// public int getTotalCount() {
+	// 	return getData(1, 1).getInt("totalCount");
+	// }
+
+}
