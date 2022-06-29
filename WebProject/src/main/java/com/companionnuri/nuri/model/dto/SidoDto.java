caec48b46da55e43b7a177a5f3edb7d308782676
yyeopp <letsksy@naver.com>
2022-06-29 20:55:27 +0900

    [추가] 지역선택 API 기초적인 수준으로 완성

--- WebProject/src/main/java/com/companionnuri/nuri/model/dto/SidoDto.java
+++ WebProject/src/main/java/com/companionnuri/nuri/model/dto/SidoDto.java
@@ -1,4 +1,24 @@
 package com.companionnuri.nuri.model.dto;
 
+
 public class SidoDto {
+
+    int sidoCode;
+    String sidoName;
+
+    public int getSidoCode() {
+        return sidoCode;
+    }
+
+    public void setSidoCode(int sidoCode) {
+        this.sidoCode = sidoCode;
+    }
+
+    public String getSidoName() {
+        return sidoName;
+    }
+
+    public void setSidoName(String sidoName) {
+        this.sidoName = sidoName;
+    }
 }
