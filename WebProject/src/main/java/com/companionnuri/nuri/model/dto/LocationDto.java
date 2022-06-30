e3c5628978a5d5eb2b22a6806a724e4cad2d1442
yyeopp <letsksy@naver.com>
2022-06-30 13:17:19 +0900

    [수정] lombok 라이브러리 추가 및 적용

--- WebProject/src/main/java/com/companionnuri/nuri/model/dto/LocationDto.java
+++ WebProject/src/main/java/com/companionnuri/nuri/model/dto/LocationDto.java
@@ -1,59 +1,17 @@
 package com.companionnuri.nuri.model.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LocationDto {
 
-    int locationId;
-    int categoryId;
-    String locationName;
-    String locationAddr;
-    String locationRoadAddr;
-    String locationTel;
+    private int locationId;
+    private int categoryId;
+    private String locationName;
+    private String locationAddr;
+    private String locationRoadAddr;
+    private String locationTel;
 
-    public int getLocationId() {
-        return locationId;
-    }
-
-    public void setLocationId(int locationId) {
-        this.locationId = locationId;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getLocationName() {
-        return locationName;
-    }
-
-    public void setLocationName(String locationName) {
-        this.locationName = locationName;
-    }
-
-    public String getLocationAddr() {
-        return locationAddr;
-    }
-
-    public void setLocationAddr(String locationAddr) {
-        this.locationAddr = locationAddr;
-    }
-
-    public String getLocationRoadAddr() {
-        return locationRoadAddr;
-    }
-
-    public void setLocationRoadAddr(String locationRoadAddr) {
-        this.locationRoadAddr = locationRoadAddr;
-    }
-
-    public String getLocationTel() {
-        return locationTel;
-    }
-
-    public void setLocationTel(String locationTel) {
-        this.locationTel = locationTel;
-    }
 }
--- WebProject/src/main/java/com/companionnuri/nuri/model/dto/SidoDto.java
+++ WebProject/src/main/java/com/companionnuri/nuri/model/dto/SidoDto.java
@@ -1,24 +1,13 @@
 package com.companionnuri.nuri.model.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class SidoDto {
 
-    int sidoCode;
-    String sidoName;
+    private int sidoCode;
+    private String sidoName;
 
-    public int getSidoCode() {
-        return sidoCode;
-    }
-
-    public void setSidoCode(int sidoCode) {
-        this.sidoCode = sidoCode;
-    }
-
-    public String getSidoName() {
-        return sidoName;
-    }
-
-    public void setSidoName(String sidoName) {
-        this.sidoName = sidoName;
-    }
 }
