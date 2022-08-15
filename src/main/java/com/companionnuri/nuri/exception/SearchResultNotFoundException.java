b91176490e9f2c5b760be6a2ced6b92908f0c293
yyeopp <letsksy@naver.com>
2022-08-15 14:36:38 +0900

    [MOD] minor

--- src/main/java/com/companionnuri/nuri/exception/SearchResultNotFoundException.java
+++ src/main/java/com/companionnuri/nuri/exception/SearchResultNotFoundException.java
@@ -2,7 +2,7 @@ package com.companionnuri.nuri.exception;
 
 import lombok.extern.slf4j.Slf4j;
 
-public class SearchResultNotFoundException extends BadRequestException{
+public class SearchResultNotFoundException extends BadRequestException {
 
     public SearchResultNotFoundException() {
         super("[SORRY] 검색 결과가 존재하지 않습니다.");
